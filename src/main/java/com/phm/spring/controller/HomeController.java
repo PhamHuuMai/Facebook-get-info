23ad144bce22340835127bb1055d260a3f60c3f0
DESKTOP-FJ00PFP\MaiPH <mai.pham.intern@ntq-solution.com.vn>
2019-06-22 21:18:19 +0700

    commit

--- src/main/java/com/phm/spring/controller/HomeController.java
+++ src/main/java/com/phm/spring/controller/HomeController.java
@@ -20,7 +20,14 @@ import com.google.gson.*;
 
 import facebook4j.auth.AccessToken;
 import facebook4j.FacebookException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 @Controller
 @RequestMapping("/")
@@ -31,54 +38,57 @@ public class HomeController {
     public String action(@RequestParam(name = "code", required = false) String code) {
         String aa = "";
         try {
-//            Facebook facebook = new FacebookFactory().getInstance();
-//            facebook.setOAuthAppId("415865425661793", "bb9136f15a8bdedd3b23c555dfff7d0e");
-//            facebook.setOAuthPermissions("email,publish_stream");
-//            facebook.setOAuthCallbackURL("");
-//            //facebook.setOAuthCallbackURL(_redirectURL);
-//            AccessToken token = facebook.getOAuthAccessToken(code);
-//            String accessToken = token.getToken();
-//            if (accessToken == null) {
-//                return null;
-//            }
-//            User user = facebook.getMe();
-//            String id = user.getId();
-//            String mail = user.getEmail();
-            return "id = " + code;
+            Facebook facebook = new FacebookFactory().getInstance();
+            facebook.setOAuthAppId("415865425661793", "bb9136f15a8bdedd3b23c555dfff7d0e");
+            facebook.setOAuthPermissions("email,publish_stream");
+            facebook.setOAuthCallbackURL("");
+            //facebook.setOAuthCallbackURL(_redirectURL);
+            AccessToken token = facebook.getOAuthAccessToken(getAccessToken(code));
+            String accessToken = token.getToken();
+            if (accessToken == null) {
+                return null;
+            }
+            User user = facebook.getMe();
+            String id = user.getId();
+            String mail = user.getEmail();
+            return "id = " + id + " code = " + mail;
         } catch (Exception ex) {
             aa = ex.getLocalizedMessage();
         }
         return aa + " loi ";
     }
 
-    private String getCode(String code1, String cliSec) throws Exception {
-        Base64 base = new Base64(true);
-        String[] signedRequest2 = code1.split("\\.", 2);
-        String sig_2 = signedRequest2[1];
-        String sig2 = new String(base.decodeBase64(sig_2.getBytes("UTF-8")));
-        JsonParser parser = new JsonParser();
-        JsonElement obj = parser.parse(sig2);
-        JsonObject map = obj.getAsJsonObject();
-        if (!(map.get("algorithm")).getAsString().equals("HMAC-SHA256")) {
-            return "";
-        }
-        if (!getHmacSHA256(sig_2, cliSec).equals(signedRequest2[0])) {
+    private String getAccessToken(String code) {
 
-            return "";
-        }
-        String code = map.get("code").getAsString();
-        if (code == null) {
-            return "";
-        }
-        return "";
-    }
+        try {
+            String url = "https://graph.facebook.com/v3.3/oauth/access_token?client_id=415865425661793&redirect_uri=https://maiph.herokuapp.com/callback&client_secret=bb9136f15a8bdedd3b23c555dfff7d0e&code=";
+            url = url + code;
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            // optional default is GET
+            con.setRequestMethod("GET");
+            //add request header
+//            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = con.getResponseCode();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
 
-    private static String getHmacSHA256(String data, String key) throws Exception {
-        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
-        Mac mac = Mac.getInstance("HmacSHA256");
-        mac.init(secretKey);
-        byte[] hmacData = mac.doFinal(data.getBytes("UTF-8"));
-        String hmacSt = new String(Base64.encodeBase64URLSafe(hmacData), "UTF-8");
-        return hmacSt;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            //print result
+            System.out.println(response.toString());
+
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(response.toString());
+            String a = (String)json.get("access_token");
+            return a;
+        } catch (Exception ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
