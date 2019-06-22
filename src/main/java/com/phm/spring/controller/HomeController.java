a5af2e0087959396601462863d2f31c707c7b3a7
PhamHuuMai96 <huumaipham1996@gmail.com>
2019-06-22 10:34:09 +0700

    fdfdfdf

--- src/main/java/com/phm/spring/controller/HomeController.java
+++ src/main/java/com/phm/spring/controller/HomeController.java
@@ -1,11 +1,26 @@
 package com.phm.spring.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.User;
+import facebook4j.auth.AccessToken;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.google.gson.*;
+
+import facebook4j.auth.AccessToken;
+import facebook4j.FacebookException;
+import org.apache.tomcat.util.codec.binary.Base64;
 
 @Controller
 @RequestMapping("/")
@@ -13,20 +28,56 @@ public class HomeController {
 
     @GetMapping("callback")
     @ResponseBody
-    public String action(@RequestParam(name = "ow", required = false) String ow,
-            @RequestParam(name = "error", required = false) String error,
-            @RequestParam(name = "code", required = false) String sr,
-            HttpServletRequest request) {
-        String owAtt = (String) request.getAttribute("ow");
-        String errorAtt = (String) request.getAttribute("error");
-        String srAtt = (String) request.getAttribute("sr");
-
-        return "ow = " + ow
-                + "error = " + error
-                + "sr = " + sr
-                + "ow att = " + owAtt
-                + "error att = " + errorAtt
-                + "sr att = " + srAtt;
+    public String action(@RequestParam(name = "code", required = false) String code) throws Exception {
+        try {
+            Facebook facebook = new FacebookFactory().getInstance();
+            facebook.setOAuthAppId("415865425661793", "bb9136f15a8bdedd3b23c555dfff7d0e");
+            facebook.setOAuthPermissions("email,publish_stream");
+            facebook.setOAuthCallbackURL("");
+            //facebook.setOAuthCallbackURL(_redirectURL);
+            AccessToken token = facebook.getOAuthAccessToken(getCode(code,"bb9136f15a8bdedd3b23c555dfff7d0e"));
+            String accessToken = token.getToken();
+            if (accessToken == null) {
+                return null;
+            }
+            User user = facebook.getMe();
+            String id = user.getId();
+            String mail = user.getEmail();
+            return "id = " + id + " = " + mail;
+        } catch (FacebookException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "Lỗi con mẹ nó rồi ahihi";
     }
 
+    private String getCode(String code1, String cliSec) throws Exception {
+        Base64 base = new Base64(true);
+        String[] signedRequest2 = code1.split("\\.", 2);
+        String sig_2 = signedRequest2[1];
+        String sig2 = new String(base.decodeBase64(sig_2.getBytes("UTF-8")));
+        JsonParser parser = new JsonParser();
+        JsonElement obj = parser.parse(sig2);
+        JsonObject map = obj.getAsJsonObject();
+        if (!(map.get("algorithm")).getAsString().equals("HMAC-SHA256")) {
+            return "";
+        }
+        if (!getHmacSHA256(sig_2, cliSec).equals(signedRequest2[0])) {
+
+            return "";
+        }
+        String code = map.get("code").getAsString();
+        if (code == null) {
+            return "";
+        }
+        return "";
+    }
+
+    private static String getHmacSHA256(String data, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(secretKey);
+        byte[] hmacData = mac.doFinal(data.getBytes("UTF-8"));
+        String hmacSt = new String(Base64.encodeBase64URLSafe(hmacData), "UTF-8");
+        return hmacSt;
+    }
 }
